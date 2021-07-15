365226d33cb8753b40913eafdaf4e4dcb1fc311a
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-07-14 20:06:56 -0500

    changed html router

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const apiRoute = require("./routes/apiRoute");
-const htmlRoute = require("./routes/htmlRoute");
+// const htmlRoute = require("./routes/htmlRoute");
 const path = require("path");
 const router = express.Router()
 
 // Start app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001
-htmlRoute(router);
+// htmlRoute(router);
+router.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+
+router.get('/notes',function(req,res) {
+    res.sendFile(path.join(__dirname+'/public/notes.html'));
+});
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
