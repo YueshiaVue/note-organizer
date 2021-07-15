4be47b280782346b813a4a3334aa7e2932c32f64
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-07-14 20:10:46 -0500

    .

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const apiRoute = require("./routes/apiRoute");
+// const apiRoute = require("./routes/apiRoute");
 // const htmlRoute = require("./routes/htmlRoute");
 const path = require("path");
 const router = express.Router()
@@ -8,6 +8,9 @@ const router = express.Router()
 const app = express();
 const PORT = process.env.PORT || 3001
 // htmlRoute(router);
+const uniqId = require("uniqId");
+let database = require("./db/noteData");
+
 router.get('/',function(req,res) {
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
@@ -22,7 +25,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-apiRoute(app);
+// apiRoute(app);
+
+  app.get("/api/notes",(req,res) => {
+            res.send(database);
+    });
+
+    app.delete("/api/notes",(req,res)=>{
+            let id = req.body.id;
+            database = database.filter((obj)=>{
+                return obj.id !== id;
+            })
+            res.send(database);
+    })
+
+    app.post("/api/notes",(req,res)=> {
+            let data = req.body;
+            data.id = uniqId();
+            database.push(data);
+            res.send(database);
+    });
 
 app.use('/', router);
 
